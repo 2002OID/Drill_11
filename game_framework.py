32b44ddcf0e532ad2b8dffbc1740895310532813
2002OID <ykkim308321@gmail.com>
2023-11-02 10:42:40 +0900

    프레인시간에 따른 액션 프레임 설정

--- game_framework.py
+++ game_framework.py
@@ -53,7 +53,7 @@ def run(start_mode):
         frame_time = time.time() - current_time
         frame_rate = 1.0/frame_time
         current_time += frame_time
-        print(f'Frame time :{frame_time}, Frame Rate : {frame_rate}')
+        #print(f'Frame time :{frame_time}, Frame Rate : {frame_rate}')
 
     # repeatedly delete the top of the stack
     while (len(stack) > 0):
